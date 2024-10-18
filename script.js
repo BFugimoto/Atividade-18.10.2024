e0a229c87e4a106b8984e36eb69a93926ad0b355
BFugimoto <brunofujimoto98@gmail.com>
2024-10-18 08:45:28 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const data = document.getElementById("data");
+const hora = document.getElementById("hora");
+let atualizar = document.getElementById("atualizar");
+
+const nowDate = new Date();
+
+data.innerHTML = nowDate.getFullYear() + "/" + nowDate.getMonth() + 1 + "/" + nowDate.getDay();
+
+hora.innerHTML = nowDate.getHours() + ":" + nowDate.getSeconds();
+
+atualizar.addEventListener("click", () =>{
+    location.reload();
+});
